5c3b7327d188da68a5481e87f0e853c8cd71abcd
chrod <chrod.dev@gmail.com>
2024-05-09 16:16:54 -0500

    Update filter input placeholder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,43 +3,41 @@ import { useCoinsContext } from "../context/CoinsContext";
 import { ClipLoader } from "react-spinners";
 
 function Navbar() {
-    const { setFilter } = useCoinsContext();
-    const [isTyping, setIsTyping] = useState(false);
+  const { setFilter } = useCoinsContext();
+  const [isTyping, setIsTyping] = useState(false);
 
-    const handleChange = (e) => {
-        setIsTyping(true);
-        const { value } = e.target;
-        setFilter(value);
-        setTimeout(() => {
-            setIsTyping(false);
-        }, 750);
-    }
+  const handleChange = (e) => {
+    setIsTyping(true);
+    const { value } = e.target;
+    setFilter(value);
+    setTimeout(() => {
+      setIsTyping(false);
+    }, 750);
+  };
 
-    return (
-        <header className="navbar">
-            <img
-                src="/crypto-logo.png"
-                className="navbar__logo"
-            />
-            <div className="navbar__filter">
-                <input
-                    className="form-control"
-                    placeholder="Filter..." type="text"
-                    onChange={handleChange}
-                />
-                {isTyping &&
-                    <ClipLoader
-                        color="#303030"
-                        size={20}
-                        cssOverride={{
-                            position: "absolute",
-                            right: ".7rem"
-                        }}
-                    />
-                }
-            </div>
-        </header>
-    )
+  return (
+    <header className="navbar">
+      <img src="/crypto-logo.png" className="navbar__logo" />
+      <div className="navbar__filter">
+        <input
+          className="form-control"
+          placeholder="Filter coin.."
+          type="text"
+          onChange={handleChange}
+        />
+        {isTyping && (
+          <ClipLoader
+            color="#303030"
+            size={20}
+            cssOverride={{
+              position: "absolute",
+              right: ".7rem",
+            }}
+          />
+        )}
+      </div>
+    </header>
+  );
 }
 
 export default Navbar;
